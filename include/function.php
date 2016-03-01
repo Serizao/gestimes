2e58a751ed068821f019fb3651bc43c6c8fb6559
serizao <william.leberre@gmail.com>
2016-03-01 16:51:47 +0100

    add function

--- include/function.php
+++ include/function.php
@@ -10,14 +10,19 @@ function secureAccess(){
 }                                                                                                            
 function checkAccess(){
 	$bdd=new bdd;
-	$username=array($_SESSION['username']);
+	$username=array('serizao');
 	$result=$bdd->tab('select * from user where username=?', $username );                                                                                      
     return ($result);                                                                     
 }   
 
+function last_time($id, $date){
+	$bdd=new bdd;
+	$data=array($id);
+	$result=$bdd->tab("SELECT es, DATE_FORMAT(`temps`, '%d %M %Y') AS date, DATE_FORMAT(temps, '%H:%i') AS time, temps FROM `es` WHERE cast(temps as date)='".$date."' and id_user=? order by temps asc", $data);
+	return $result;
+}
 
-
-function current_semaine($i){
+function current_semaine($i, $o){
 	date_default_timezone_set('Europe/Paris');
 	$jour = array("Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi"); 
 	$mois = array("","Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre");
@@ -31,15 +36,24 @@ function current_semaine($i){
 	 $year = date('Y');
 		 for($i = 1; $i <= 365; $i++) {
 		  	$week = date("W", mktime(0, 0, 0, 1, $i, $year));
+		  	if(isset($o) and $o!=0) $w = $o;
+		  	
+		  	$semaine['n']=$w;
 			  if($week == $w) {
 			    
 			    // Ensuite pour afficher tous les jours de la semaine
 					
 				    for($d = 0; $d < 7; $d++) {
-				    	$semaine['jour'][]=$jour[date("w", mktime(0, 0, 0, 1, $i+$d, $year))] ;
-				    	$semaine['numero'][]=date("d", mktime(0, 0, 0, 1, $i+$d, $year)); 
-						$semaine['mois'][]=$mois[date("n", mktime(0, 0, 0, 1, $i+$d, $year))] ;
-						$semaine['annee'][]=$year;
+				    	$semaine['jour'][$d]=$jour[date("w", mktime(0, 0, 0, 1, $i+$d, $year))] ;
+				    	$semaine['numero'][$d]=date("d", mktime(0, 0, 0, 1, $i+$d, $year)); 
+						$semaine['mois'][$d]=$mois[date("n", mktime(0, 0, 0, 1, $i+$d, $year))] ;
+						$semaine['mois2'][$d]=date("m", mktime(0, 0, 0, 1, $i+$d, $year)) ;
+						$semaine['annee'][$d]=$year;
+						$date=$semaine['annee'][$d].'-'.$semaine['mois2'][$d].'-'.$semaine['numero'][$d];
+						
+						$result=last_time('0', $date );
+						$semaine[$d]=$result[0];
+
 				    }
 			   return $semaine;
 			   break;
@@ -50,8 +64,9 @@ function current_semaine($i){
 }
 
 function arriver(){
+	date_default_timezone_set('Europe/Paris');
 	$bdd=new bdd;
-	$data=array('','e','',date('Y-m-d H:m:s'));
+	$data=array('','e','',date('Y-m-d H:i:s'));
 	$result=$bdd->tab('insert into `es`(`id`, `es`, `id_user`, `temps`) VALUES( ?, ?, ?, ?) ', $data);
 	
 
@@ -59,7 +74,23 @@ function arriver(){
 function partir(){
 	date_default_timezone_set('Europe/Paris');
 	$bdd=new bdd;
-	$data=array('','s','',date('Y-m-d H:m:s'));
+	$data=array('','s','',date('Y-m-d H:i:s'));
 	$result=$bdd->tab('insert into `es`(`id`, `es`, `id_user`, `temps`) VALUES( ?, ?, ?, ?) ', $data);
+}
+
+
+function calcul_time($h1){
+	$spend=0;
+	$table=array();
+	for($p=1;$p<count($h1);$p++){
+		$i=$p-1;
+		$spend=$spend+($h1[$p]-$h1[$i]);
+		$table['heure']=intval($spend / 3600);
+		$table['minutes']=intval(($spend % 3600) / 60);
+		$table['second']=intval((($spend% 3600) % 60));
+		
+		$p++;
+	}
+	return $table;
 }
 ?>
--- index.php
+++ index.php
@@ -8,32 +8,66 @@
 </head>
 <body>
 <?php
+date_default_timezone_set('Europe/Paris');
     include_once('include/bdd.php');
     include_once('include/function.php');
 
     secureAccess();
     include_once ('include/top-barre.php');
     include_once ('include/ajax.php');
- 
-	$result=current_semaine('');
+    $nav=0;
+    $bdd=new bdd;
+    $data=array('0');
+    $result=$bdd->tab('select * from es where id_user=? ', $data);
+    for($w=0;$w<count($result[0]);$w++){
+       $yeartime[]=strtotime($result[0][$w]['temps']);
+    }
+
+   $yeartime=calcul_time($yeartime);
+
+
+ if(isset($_REQUEST['semaine']))$nav=$_REQUEST['semaine'];
+	$result=current_semaine('', $nav);
 	$nbj=count($result['jour']);
+  $semaine=$result['n']-1;
+  $semaine2=$result['n']+1;
+  echo "<a href='index.php?semaine=".$semaine."' style='float:right'> semaine precedente ></a>";
+  echo "<a href='index.php?semaine=".$semaine2."' style='float:left'>< semaine suivante </a>";
 	echo '<table style="width:100%">
   			<tr>' ;
   	for($i=0;$i<$nbj;$i++){
   		$now1='';
   		$now2='';
-		$color='';
+		  $color='';
+      $time='';
+      $total=array();
   		if($result['jour'][$i]=='Samedi' or $result['jour'][$i]=='Dimanche' or $result['numero'][$i] < date('j')) $color= "style='background-color:#67809F'";
-  		if ($result['numero'][$i]==date('j')){
+  		if ($result['numero'][$i]==date('j') and $result['mois2'][$i]==date('n')){
   			$now1='class="now-ar"';
   			$now2='class="now-par"';
   			$color= "style='background-color:#87D37C'";
 		}  		
-
-  		echo'<td '.$color.'>'. $result['jour'][$i].' '.$result['numero'][$i].' '.$result['mois'][$i].'<br>('.$result['annee'][$i].')<br><div '.$now1.' id="arive"></div><br><div '.$now2.' id="depart"></div><br><div  id="total"></div></td>';
-  	}
+   for($o=0;$o<count($result[$i]);$o++){
+      if(isset($result[$i][$o]) and !empty($result[$i][$o])){
+        if($result[$i][$o]['es']=="e"){
+          $time=$time.'<br>Arrivé à '.$result[$i][$o]['time'];
+          $total[]=strtotime($result[$i][$o]['temps']);
+        }
+        if($result[$i][$o]['es']=="s"){
+          $time=$time.'<br>Parti à '.$result[$i][$o]['time'];
+          $total[]=strtotime($result[$i][$o]['temps']);
+        }
+      }
+    }
+    $tempspasser=calcul_time($total);
+    $finaltime='';
+    if(isset($tempspasser) and !empty($tempspasser['second'])) $finaltime=$tempspasser['heure'].'h'.$tempspasser['minutes'].'min'.$tempspasser['second'];
+  		echo'<td '.$color.'>'. $result['jour'][$i].' '.$result['numero'][$i].' '.$result['mois'][$i].'<br>('.$result['annee'][$i].')<br>'.$time.'<div '.$now1.' id="arrive"></div><br><div '.$now2.' id="depart"></div><br><div  id="total">'.$finaltime.'</div></td>';
+    
+    }
+  	
   	echo '</tr></table>';
-
+echo $yeartime['heure'].'h'.$yeartime['minutes'].'min'.$yeartime['second'] .'au total';
   	
 ?>
 <script type="text/javascript">
