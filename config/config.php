6fed163aa1062004304044ca0020499c19ff5909
wleberre <wleberre@users.noreply.github.com>
2016-06-25 16:01:15 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -2,17 +2,17 @@
 
 
 	/* parmètre de connection au serveur ldap */
-	$host_ldap="ldap://bruz.triskem.fr";
-	$domain_ldap="DOM2003";
-	$dn_ldap="dc=DOM2003,dc=local";
+	$host_ldap="";
+	$domain_ldap="";
+	$dn_ldap="";
 
 
 
 
 	/* parmètre de connection a la base de donnée */
 	$host_bdd = "127.0.0.1";
-    $user_bdd = "root";
-    $pass_bdd = "7ac@RZ4";
+    $user_bdd = "";
+    $pass_bdd = "";
     $base_bdd = "time"; 
 
 /*possibilité de rentré les login de l'ad en dur afin de pas les rentrer utltérieurement*/
