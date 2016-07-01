0d9417203e94f7b050f59d5cbbfafdd97a0b3a52
serizao <william.leberre@gmail.com>
2016-07-01 10:47:25 +0200

    fix bug+update style

--- config/config.php
+++ config/config.php
@@ -2,9 +2,9 @@
 
 
 	/* parmètre de connection au serveur ldap */
-	$host_ldap="ldap://bruz.triskem.fr";
-	$domain_ldap="DOM2003";
-	$dn_ldap="dc=DOM2003,dc=local";
+	$host_ldap="";
+	$domain_ldap="";
+	$dn_ldap="dc=exemple,dc=local";
 
 
 
@@ -12,7 +12,7 @@
 	/* parmètre de connection a la base de donnée */
 	$host_bdd = "127.0.0.1";
     $user_bdd = "root";
-    $pass_bdd = "7ac@RZ4";
+    $pass_bdd = "root";
     $base_bdd = "time"; 
 
 /*possibilité de rentré les login de l'ad en dur afin de pas les rentrer utltérieurement*/
