11f26a273e3321b0e9e8c31ccae5b93727bf3d43
serizao <william.leberre@gmail.com>
2016-07-04 15:58:21 +0200

    code syntaxe

--- config/config.php
+++ config/config.php
@@ -1,11 +1,11 @@
 <?php
 /* parmètre de connection au serveur ldap */
-$host_ldap="";
-$domain_ldap="";
-$dn_ldap="dc=exemple,dc=local";
+$host_ldap='';
+$domain_ldap='';
+$dn_ldap='dc=exemple,dc=local';
 /* parmètre de connection a la base de donnée */
-$host_bdd = "127.0.0.1";
-$user_bdd = "root";
-$pass_bdd = "root";
-$base_bdd = "t2"; 
+$host_bdd = '127.0.0.1';
+$user_bdd = 'root';
+$pass_bdd = 'root';
+$base_bdd = 't2'; 
 ?>
