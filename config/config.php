041a7b2b1f076252f41ca3fc7038c6e43e338a3f
serizao <william.leberre@gmail.com>
2016-07-11 12:04:56 +0200

    code syntaxe

--- config/config.php
+++ config/config.php
@@ -1,14 +1,14 @@
 <?php
 if (!defined('BASE_BDD')){
-	//bdd
-	define('BASE_BDD','time');
-	define('USER_BDD','');
-	define('PASS_BDD','');
-	define('HOST_BDD','127.0.0.1');
-	//session
-	define('INACTIVITY_TIMEOUT',900);
-	//ldap
-	define('HOST_LDAP','');
-	define('DOMAIN_LDAP','exemple.com');
-	define('DN_LDAP','dc=exemple,dc=com');
+    //bdd
+    define('BASE_BDD','time');
+    define('USER_BDD','');
+    define('PASS_BDD','');
+    define('HOST_BDD','127.0.0.1');
+    //session
+    define('INACTIVITY_TIMEOUT',900);
+    //ldap
+    define('HOST_LDAP','');
+    define('DOMAIN_LDAP','exemple.com');
+    define('DN_LDAP','dc=exemple,dc=com');
 }
