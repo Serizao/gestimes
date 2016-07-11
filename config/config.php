b64a7d92b179811136937ff9c755793664fc0d18
wleberre <wleberre@users.noreply.github.com>
2016-07-11 11:55:11 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -4,14 +4,13 @@ if (!defined('BASE_BDD')){
 	define('BASE_BDD','time');
 	define('USER_BDD','');
 	define('PASS_BDD','');
-	define('HOST_BDD','');
+	define('HOST_BDD','127.0.0.1');
 	//session
 	define('INACTIVITY_TIMEOUT',900);
 	//ldap
 	define('HOST_LDAP','');
-	define('DOMAIN_LDAP','');
-	define('DN_LDAP','');
-
+	define('DOMAIN_LDAP','exemple.com');
+	define('DN_LDAP','dc=exemple,dc=com');
 }
 
 
